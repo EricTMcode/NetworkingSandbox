af3af1350d51254949d1a74638365d8893e4024a
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-06-27 13:51:24 +0200

    Initial Commit

---
+++ NetworkingSandbox/NetworkingSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingSandboxApp.swift
+//  NetworkingSandbox
+//
+//  Created by Eric on 27/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
